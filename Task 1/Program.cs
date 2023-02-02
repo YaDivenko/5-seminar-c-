69c47c853a5fa89889e453479ea211de3699dd98
Yana <yadivenko@gmail.com>
2023-02-02 16:12:16 +0300

    1

---
+++ Task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(100,1000);
+}
+
+foreach (var item in arr)
+{
+    System.Console.Write(item + " ");
+}
+int count = 0;
+
+for (int j = 0; j < arr.Length; j++)
+if (arr[j] % 2 == 0)
+count++;
+
+Console.WriteLine("\n" + $"четных чисел {count}");
+
